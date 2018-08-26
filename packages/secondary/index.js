8125106e5faa4fe957a35840d70409265de48a70
Daniel Flynn <djflynn15@gmail.com>
2018-08-26 16:14:03 -0400

    feat: Update secondary

--- packages/secondary/index.js
+++ packages/secondary/index.js
@@ -5,7 +5,12 @@ export default class Secondary {
     this.Main = new Main();
   }
 
+  log() {
+    console.log('Hi!');
+  }
+
   render() {
     console.log(this.Main.render());
+    this.log();
   }
 }
